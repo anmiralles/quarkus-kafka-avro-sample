22526edaba77f909e1b9a912be65bed52db1dd43
Angel Miralles <anmiralles@yahoo.es>
2023-04-13 10:41:30 +0200

    feat: implementing polymorphic processing

--- src/main/java/me/amiralles/MovieConsumer.java
+++ src/main/java/me/amiralles/MovieConsumer.java
@@ -1,5 +1,7 @@
 package me.amiralles;
 
+import me.amiralles.quarkus.MovieCreated;
+import me.amiralles.quarkus.MovieUpdated;
 import org.apache.avro.generic.GenericRecord;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
@@ -12,9 +14,21 @@ public class MovieConsumer {
     private static final Logger LOGGER = Logger.getLogger("MovieConsumer");
 
     @Incoming("movies-from-kafka")
-    public void receive(GenericRecord movie) {
-        String name = movie.getSchema().getFullName();
-        LOGGER.infof("Received movie: %s", name);
+    public void receive(Object movie) {
+
+        if(movie instanceof MovieCreated) {
+            handle((MovieCreated) movie);
+        } else if(movie instanceof MovieUpdated) {
+            handle((MovieUpdated) movie);
+        }
+    }
+
+    public void handle(MovieCreated event) {
+        LOGGER.infof("Movie created: %s", event);
+    }
+
+    public void handle(MovieUpdated event) {
+        LOGGER.infof("Movie updated: %s", event);
     }
 
 }
